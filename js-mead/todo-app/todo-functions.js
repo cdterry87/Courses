fd4505edc0c982a474534ca0f0f8874a8a6583dd
= <chase.terry87@gmail.com>
2019-11-11 21:27:55 -0600

    Made the delete button work for notes and todos.

--- js-mead/todo-app/todo-functions.js
+++ js-mead/todo-app/todo-functions.js
@@ -2,7 +2,7 @@
 const getSavedTodos = function() {
     const todosJSON = localStorage.getItem('todos')
 
-    if (todosJSON !== null) {
+    if (todosJSON !== null && todosJSON !== 'undefined') {
         return JSON.parse(todosJSON)
     } else {
         return []
@@ -12,6 +12,17 @@ const getSavedTodos = function() {
 // Save notes to localstorage
 const saveTodos = function(todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Delete a note
+const deleteTodo = function(id) {
+    const todoIndex = todos.findIndex(function(todo) {
+        return todo.id === id
+    })
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
 }
 
 // Render application notes
@@ -35,12 +46,29 @@ const renderTodos = function (todos, filters) {
     })
 }
 
+
 // Generate DOM structure for a note
 const generateTodoDOM = function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
+    const div = document.createElement('div')
+    const todoText = document.createElement('span')
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    const button = document.createElement('button')
+    button.textContent = 'Delete'
+    button.addEventListener('click', function() {
+        deleteTodo(todo.id)
+        saveTodos()
+        renderTodos(todos, filters)
+    })
 
-    return p
+    div.appendChild(checkbox)
+    todoText.textContent = todo.text
+    todoText.appendChild(button)
+
+
+    div.appendChild(todoText)
+
+    return div
 }
 
 // Get the DOM elements for a list summary
