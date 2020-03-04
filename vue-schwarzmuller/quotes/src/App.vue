87ad1506b2524cdb768aa492e14861a72d285a17
Chase Terry <chase.terry87@gmail.com>
2020-03-04 17:43:30 -0600

    Created the quotes application using slots.

---
+++ vue-schwarzmuller/quotes/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes" />
+        <NewQuote @quoteAdded="newQuote" />
+        <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></QuoteGrid>
+
+        <div class="row">
+            <div class="text-center col-sm-12">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        components: {
+            Header,
+            NewQuote,
+            QuoteGrid,
+        },
+        data() {
+            return {
+                quotes: [
+                    'Just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete a quote first.')
+                }
+                this.quotes.push(quote)
+            },
+            deleteQuote(index) {
+                console.log('delete ' + index)
+                this.quotes.splice(index, 1)
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ vue-schwarzmuller/quotes/src/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form @submit.prevent="createNew">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="quote">Quote</label>
+                <textarea id="quote" class="form-control" cols="30" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote)
+                this.quote = ''
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
