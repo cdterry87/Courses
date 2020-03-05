5801eb9821438d076d94a359f4bc8b4df0c0364d
Chase Terry <chase.terry87@gmail.com>
2020-03-05 17:08:49 -0600

    Added save adnd load functions and hitting firebase with vue-resource.

--- vue-schwarzmuller/stocks/src/components/Header.vue
+++ vue-schwarzmuller/stocks/src/components/Header.vue
@@ -19,8 +19,8 @@
                         <li class="dropdown" @click="isDropdownOpen = !isDropdownOpen" :class="{open: isDropdownOpen}">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save / Load <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Save Data</a></li>
-                                <li><a href="#">Load Data</a></li>
+                                <li><a href="#" @click="saveData()">Save Data</a></li>
+                                <li><a href="#" @click="loadData()">Load Data</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -45,11 +45,23 @@
             }
         },
         methods: {
-            ...mapActions([
-                'randomizeStocks'
-            ]),
+            ...mapActions({
+                randomizeStocks: 'randomizeStocks',
+                fetchData: 'loadData'
+            }),
             endDay() {
                 this.randomizeStocks()
+            },
+            saveData() {
+                const data = {
+                    funds: this.$store.getters.funds,
+                    stockPortfolio: this.$store.getters.stockPortfolio,
+                    stocks: this.$store.getters.stocks
+                }
+                this.$http.put('data.json', data)
+            },
+            loadData() {
+                this.fetchData()
             }
         },
     }
