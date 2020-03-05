3ddb3a9694d9a9c10163a6f7422a5647685ea8ec
Chase Terry <chase.terry87@gmail.com>
2020-03-05 16:17:43 -0600

    Added transitions between routes.

--- vue-schwarzmuller/stocks/src/App.vue
+++ vue-schwarzmuller/stocks/src/App.vue
@@ -4,7 +4,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <router-view />
+                    <transition name="slide" mode="out-in">
+                        <router-view />
+                    </transition>
                 </div>
             </div>
         </div>
@@ -26,5 +28,38 @@
 </script>
 
 <style>
+    body {
+        padding: 30px;
+    }
 
+    .slide-enter-active {
+        animation: slide-in .3s ease-in forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out .3s ease-in forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slide=out {
+        from {
+            opacity: 1;
+            transform: translateY(0);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateY(-30px);
+        }
+    }
 </style>
