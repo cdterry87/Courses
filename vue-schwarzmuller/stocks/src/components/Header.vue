1ff6cd68709fac8913fcd9e2579e607bee5319a6
Chase Terry <chase.terry87@gmail.com>
2020-03-05 10:19:51 -0600

    Switched to bootstrap 3

--- vue-schwarzmuller/stocks/src/components/Header.vue
+++ vue-schwarzmuller/stocks/src/components/Header.vue
@@ -1,31 +1,28 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <router-link to="/" class="navbar-brand">Brand</router-link>
+                </div>
 
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item" activeClass="active">
-                        <router-link class="nav-link" to="/portfolio">Portfolio</router-link>
-                    </li>
-                    <li class="nav-item" activeClass="active">
-                        <router-link class="nav-link" to="/stocks">Stocks</router-link>
-                    </li>
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" tag="li" activeClass="active"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" tag="li" activeClass="active"><a>Stocks</a></router-link>
                 </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">End Day</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown link
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Save Data</a>
-                            <a class="dropdown-item" href="#">Load data</a>
-                        </div>
-                    </li>
-                </ul>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">End Day</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save / Load <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Save Data</a></li>
+                                <li><a href="#">Load Data</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </div>
