5584a30f7e12219624069ce4ad9b72eedf2b0376
Chase Terry <chase.terry87@gmail.com>
2020-03-05 16:17:58 -0600

    Made dropdown work.

--- vue-schwarzmuller/stocks/src/components/Header.vue
+++ vue-schwarzmuller/stocks/src/components/Header.vue
@@ -16,7 +16,7 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#" @click="endDay">End Day</a></li>
-                        <li class="dropdown">
+                        <li class="dropdown" @click="isDropdownOpen = !isDropdownOpen" :class="{open: isDropdownOpen}">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save / Load <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Save Data</a></li>
@@ -34,6 +34,11 @@
     import {mapActions} from 'vuex'
 
     export default {
+        data() {
+            return {
+                isDropdownOpen: false
+            }
+        },
         computed: {
             funds() {
                 return this.$store.getters.funds
