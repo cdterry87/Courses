7ab94b2f6fbac7a978e50d1f7bd2b442649bbaab
Chase Terry <chase.terry87@gmail.com>
2020-05-05 22:59:05 -0500

    Added form inputs in svelte course.

--- svelte-trent/svelte-library/src/App.svelte
+++ svelte-trent/svelte-library/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
-	import { Route, Router } from 'svelte-routing'
+  import { Route, Router } from "svelte-routing";
+  import Create from "./create/Create.svelte";
   import Detail from "./detail/Detail.svelte";
   import Library from "./library/Library.svelte";
 
-	export let url = ''
+  export let url = "";
 </script>
 
 <style>
@@ -11,12 +12,15 @@
 </style>
 
 <Router {url}>
-	<main>
-		<Route path="/books/:id" let:params>
-			<Detail id={params.id} />
-		</Route>
-		<Route path="/">
-			<Library />
-		</Route>
-	</main>
+  <main>
+    <Route path="/create">
+      <Create />
+    </Route>
+    <Route path="/books/:id" let:params>
+      <Detail id={params.id} />
+    </Route>
+    <Route path="/">
+      <Library />
+    </Route>
+  </main>
 </Router>
---
+++ svelte-trent/svelte-library/src/create/Create.svelte
@@ -0,0 +1,58 @@
+<script>
+  import BackButtonRow from "../common/BackButtonRow.svelte";
+  import BookCover from "../common/BookCover.svelte";
+  import Button from "../common/Button.svelte";
+  import Header from "../common/Header.svelte";
+  import { httpPost } from "../common/api.js";
+  import TextInput from "./TextInput.svelte";
+
+  let title = "";
+  let author = "";
+  let cover = "";
+  let about = "";
+
+  // Destiny operator; runs this code whenever value in memory changes
+  $: console.log({ title, author, cover, about });
+</script>
+
+<style>
+  form {
+    display: grid;
+    grid-auto-rows: auto;
+    grid-template-columns: 1fr;
+    gap: var(--spacingXLarge);
+  }
+  .fields {
+    display: grid;
+    grid-auto-rows: auto;
+    gap: var(--spacingMedium);
+  }
+  .preview {
+    display: grid;
+    grid-template-columns: minmax(20vw, 10rem);
+    grid-template-rows: minmax(32vw, 16rem);
+  }
+  @media (min-width: 48rem) {
+    form {
+      grid-template-columns: 60vw 20vw;
+    }
+  }
+</style>
+
+<BackButtonRow />
+
+<Header element="h1" size="large">Create</Header>
+
+<form>
+  <div class="fields">
+    <TextInput label="Title" bind:value={title} />
+    <TextInput label="Author" bind:value={author} />
+    <TextInput label="Cover URL" bind:value={cover} />
+    <TextInput label="About" bind:value={about} multiline />
+  </div>
+
+  <div>
+    <Header>Preview</Header>
+    <div class="preview" />
+  </div>
+</form>
