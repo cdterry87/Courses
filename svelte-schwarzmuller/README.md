978868d19c30d0b18da3fe7ddd3984816d4379c3
Chase Terry <chase.terry87@gmail.com>
2020-05-06 21:48:20 -0500

    Started Svelte course by Max

---
+++ svelte-schwarzmuller/README.md
@@ -0,0 +1,2 @@
+### Svelte.js - The Complete Guide by Maximilian Schwarzmüller
+https://www.udemy.com/course/sveltejs-the-complete-guide/
---
+++ svelte-schwarzmuller/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	let name = "Chase";
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase();
+
+	function incrementAge() {
+		age += 1
+	}
+
+	function changeName() {
+		name = 'Maximilian'
+	}
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello, I am {uppercaseName}!</h1>
+<h2>I am {age} years old!</h2>
+<button on:click={incrementAge}>Increment Age</button>
+<button on:click={changeName}>Change Name</button>
