0464206f789b18623cabdc4f9285fbbeba52822f
Chase Terry <chase.terry87@gmail.com>
2020-05-12 14:39:20 -0500

    Added vuex and lodash and added some basic actions, getters, and mutations.

---
+++ vue-papa/src/store/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { dataService } from '../shared';
+import { GET_HEROES } from './mutation-types';
+
+Vue.use(Vuex);
+
+const state = {
+  heroes: [],
+};
+
+const mutations = {
+  [GET_HEROES](state, heroes) {
+    state.heroes = heroes;
+  },
+};
+
+const actions = {
+  async getHeroesAction({ commit }) {
+    const heroes = await dataService.getHeroes();
+    commit(GET_HEROES, heroes);
+  },
+};
+
+const getters = {
+  getHeroById: state => id => state.heroes.find(h => h.id === id),
+};
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state,
+  mutations,
+  actions,
+  getters,
+});
