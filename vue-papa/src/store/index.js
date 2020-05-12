4cc1f67be5d4b24e6899dd69fe5ca03f3231e624
Chase Terry <chase.terry87@gmail.com>
2020-05-12 15:03:24 -0500

    Completed vue getting started heroes course.

--- vue-papa/src/store/index.js
+++ vue-papa/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { dataService } from '../shared';
-import { GET_HEROES } from './mutation-types';
+import {
+  GET_HEROES,
+  ADD_HERO,
+  UPDATE_HERO,
+  DELETE_HERO,
+} from './mutation-types';
 
 Vue.use(Vuex);
 
@@ -13,12 +18,37 @@ const mutations = {
   [GET_HEROES](state, heroes) {
     state.heroes = heroes;
   },
+  [ADD_HERO](state, hero) {
+    state.heroes.push(hero);
+  },
+  [DELETE_HERO](state, hero) {
+    state.heroes = [...state.heroes.filter(h => h.id !== hero.id)];
+  },
+  [UPDATE_HERO](state, hero) {
+    const index = state.heroes.findIndex(h => h.id === hero.id);
+    state.heroes.splice(index, 1, hero);
+    // state.heroes = [...state.heroes]; // another option for updating
+  },
 };
 
 const actions = {
+  // actions let us get context which contains:
+  // { state, getters, commit, dispatch }
   async getHeroesAction({ commit }) {
     const heroes = await dataService.getHeroes();
     commit(GET_HEROES, heroes);
+  },
+  async addHeroAction({ commit }, hero) {
+    const addedHero = await dataService.addHero(hero);
+    commit(ADD_HERO, addedHero);
+  },
+  async deleteHeroAction({ commit }, hero) {
+    const deletedHeroId = await dataService.deleteHero(hero);
+    commit(DELETE_HERO, deletedHeroId);
+  },
+  async updateHeroAction({ commit }, hero) {
+    const updatedHero = await dataService.updateHero(hero);
+    commit(UPDATE_HERO, updatedHero);
   },
 };
 
